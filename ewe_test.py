1e8ce281ba55c894020eef93cdffa7cb6e4ac92c
Mark Winter <dariantg@gmail.com>
2024-03-10 19:24:21 +0000

    Updated copyright header and requirements added simple readme

--- ewe_test.py
+++ ewe_test.py
@@ -1,3 +1,19 @@
+# Supplemental code and data for: "Extreme weather should be defined according to impacts on climate vulnerable communities"
+# Copyright (C) 2024 Mark Winter
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import numpy as np
 import pandas as pd
 import seaborn as sns
---
+++ readme.md
@@ -0,0 +1,18 @@
+# Extreme weather should be defined according to impacts on climate vulnerable communities
+This repository contains supplementary code and associated survey data released along with publication.
+
+See supplementary section of the paper for details of the analysis implemented in this code [[1](#citation)]. If the analysis or code are useful please cite the paper as detailed below.
+
+## Running the code
+To run the code install python dependencies using pip (python 3.9+ is required):
+```bash
+pip install -r requirements.txt
+```
+
+Run the code from the command line:
+```bash
+python ewe_test.py
+```
+
+## Citation
+[1] TBD Upon publication: "Extreme weather should be defined according to impacts on climate vulnerable communities"
